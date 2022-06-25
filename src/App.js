65f4c8adcc7040a529540bc3fedff973dbb81156
AswathyRajM <aswathyraj767@gmail.com>
2022-06-25 15:11:20 +0530

    timer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,112 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
 
-function App() {
+function Timer() {
+  const [minute, setMinute] = React.useState(0);
+  const [second, setSecond] = React.useState(0);
+  const [id, setId] = React.useState(0);
+  const [timer, setTimer] = React.useState({
+    start: false,
+    pause: false,
+    stop: false,
+  });
+
+  const calculateTime = (second) => {
+    let setIntervalID = setInterval(() => {
+      if (second === 59) {
+        setSecond(0);
+        setMinute((m) => m + 1);
+        if (minute === 59) setMinute(0);
+      } else {
+        second += 1;
+        setSecond(second);
+      }
+    }, 1000);
+    setId(setIntervalID);
+  };
+
+  const startTimer = () => {
+    if (timer.start) return;
+
+    setTimer({
+      start: true,
+      pause: false,
+      stop: false,
+    });
+    calculateTime(0);
+  };
+
+  const stopTimer = () => {
+    setTimer({
+      start: false,
+      pause: false,
+      stop: true,
+    });
+  };
+
+  const pauseTimer = () => {
+    if (timer.pause) {
+      setSecond(second);
+      setMinute(minute);
+      calculateTime(second);
+
+      setTimer({
+        start: true,
+        pause: false,
+        stop: false,
+      });
+    } else
+      setTimer({
+        start: false,
+        pause: true,
+        stop: false,
+      });
+  };
+
+  useEffect(() => {
+    if (!timer.start && timer.stop) {
+      clearInterval(id);
+      setSecond(0);
+      setMinute(0);
+    }
+
+    if (!timer.start && timer.pause) {
+      clearInterval(id);
+      setSecond(second);
+      setMinute(minute);
+    }
+  }, [timer]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "70vh",
+      }}
+    >
+      <div
+        style={{
+          textAlign: "center",
+        }}
+      >
+        <h1 style={{ margin: "10px" }}>
+          {minute} : {second}
+        </h1>
+        <div>
+          <button onClick={startTimer} style={{ margin: "10px" }}>
+            Start
+          </button>
+          <button onClick={pauseTimer} style={{ margin: "10px" }}>
+            {timer.pause && !timer.start ? "Resume" : "Pause"}
+          </button>
+          <button onClick={stopTimer} style={{ margin: "10px" }}>
+            Stop
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
 
-export default App;
+export default Timer;
